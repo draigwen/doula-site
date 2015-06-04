7baa2c2e95689f945bb33a88dc9eadaf2583bece
draigwen <github@draigwen.com>
2015-06-04 20:44:15 +0100

    Fix navbar

--- sgdoula/navbar.php
+++ sgdoula/navbar.php
@@ -2,17 +2,17 @@
 
 function createNav($title, $link){
    if("/".$link == $_SERVER['PHP_SELF']){
-      return "<li id="active">".$title."</li>";
+      return "<li class='active'><a href='".$link."'>".$title."</a></li>";
    }else{
-      return "<li><a href="".$link."">".$title."</a></li>";
+      return "<li><a href='".$link."'>".$title."</a></li>";
    }
 }
 
 echo createNav("Home", "index-test.php");
 echo createNav("About", "about.php");
-echo createNav("Services", "services.php">
-echo createNav("Library", "library.php">
-echo createNav("Testimonials", "testimonials.php">
-echo createNav("Contact", "contact.php">
+echo createNav("Services", "services.php");
+echo createNav("Library", "library.php");
+echo createNav("Testimonials", "testimonials.php");
+echo createNav("Contact", "contact.php");
 
 ?>
