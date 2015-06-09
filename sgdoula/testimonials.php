083de8f6281a2e5dd076a1bd83344b09b558068a
draigwen <github@draigwen.com>
2015-06-09 19:11:42 +0100

    Testing out a new header and php include

--- sgdoula/testimonials.php
+++ sgdoula/testimonials.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Samantha Gadsden Doula</title>
+    <title>Samantha Gadsden Doula - Testimonials</title>
 
     <!-- Bootstrap -->
     <link href="stylesheets/styles.css" rel="stylesheet">
@@ -47,27 +47,9 @@
           </ul></div>
       </div>
     </nav></div>
-<!--    <div class="container" role="header">
-      <div class="row">
-        <div class="col-md-5">
-          <img src="images/samg.png">
-        </div>
-        <div class="col-md-7">
-          <div class="col-md-12">
-            <div class="jumbotron">
-              <h1>Samantha Gadsden <small>Birth, ante and post natal doula covering south Wales</small></h1>
-              <p class="well">mothering the mother</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
--->
     <div class="container" role="main">
+<?php include 'title.php'; ?>
         <div class="row">
-<div class="col-xs-12">
-<div class-"jumbotron"><h1>Samantha Gadsden <small>Birth, ante and post natal doula covering south Wales</small></h1></div>
-</div>
             <div class="col-xs-12">
 
                 <h2>Testimonials</h2>
---
+++ sgdoula/title.php
@@ -0,0 +1,9 @@
+<div class="row">
+  <div class="col-xs-12">
+    <div class="row">
+      <div class="col-md-2"><img src="images/samg-small.png" alt=""/></div>
+      <div class="col-md-10"><h1>Samantha Gadsden <small>Birth, ante and post natal doula covering south Wales</small></h1></div>
+    </div>
+  </div>
+</div>
+
